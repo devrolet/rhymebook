2951f2f3f7db80ddf4d9198e4f557596b9be575c
Chet Hill <chetdhill@gmail.com>
2018-08-26 20:48:13 -0400

    Add 3rd Party Module Lodash

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+// Check to see if variable is a string
+// console.log(_.isString(true));
+// console.log(_.isString('Chet'));
 
-var user = os.userInfo();
+var filteredArray = _.uniq(['Chet', 5, 'Chet', 1, 2, 3, 4]);
+console.log(filteredArray);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
 
-console.log('Result: ', notes.add(42, 39));
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+
+// console.log('Result: ', notes.add(42, 39));
