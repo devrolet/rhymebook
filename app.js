6455235ee9a64d70c2a90907442578a2a143935a
Chet Hill <chetdhill@gmail.com>
2018-08-26 20:32:34 -0400

    Create Add Function

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
+
+var res = notes.addNote();
+console.log(res);
 
 var user = os.userInfo();
 
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+console.log('Result: ', notes.add(42, 39));
